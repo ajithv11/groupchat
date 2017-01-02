16e15ea828cfc27f0e17638d58b31df07c7273fb
ajithv11 <ajithv11@gmail.com>
2017-01-02 17:42:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
+
+
+
+
